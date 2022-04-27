5e05f6da4efde7191fcec99fe2d421cff8013081
bebeji-nappa <masadecade@gmail.com>
2022-04-28 01:06:32 +0900

    Add Vue

---
+++ src/Richmd.vue
@@ -0,0 +1,39 @@
+<template>
+    <div 
+      v-html="compiledMarkdown"
+      :text="text"
+    />
+</template>
+<script>
+import { richmd } from 'richmd'
+import 'richmd/richmd.css'
+
+export default {
+  name: "Richmd",
+  props: {
+    text: {
+      type: String,
+      default: "",
+      require: true,
+    },
+    className: {
+      type: String,
+      default: "",
+    },
+    id: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      markdownText: ''
+    }
+  },
+  computed: {
+    compiledMarkdown () {
+      return richmd(this.markdownText)
+    }
+  },
+}
+</script>
